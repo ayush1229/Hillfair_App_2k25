9e25595eafc4f9bc4a7c42419609721c84639a1e
unknown <abpb2007@gmail.com>
2025-09-12 16:13:08 +0530

    created uihelper.dart

---
+++ lib/widgets/uihelper.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class UiHelper {
+  
+  static Image CustomImage({required String img}) {
+    return Image.asset("assets/images/$img");
+  }
+
+  static Text CustomText({
+    required String text,
+    required Color color,
+    required FontWeight fontweight,
+    String? fontfamily,
+    required double fontsize,
+  }) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontsize,
+        fontFamily: fontfamily ?? "regular",
+        fontWeight: fontweight,
+        color: color,
+      ),
+    );
+  }
+}
